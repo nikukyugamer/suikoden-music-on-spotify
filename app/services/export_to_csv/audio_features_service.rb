4a870baca4f581349c4ad0cc35dddfd38ade7698
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-11-28 20:47:50 +0900

    feat: 🎸 audio_features を取得するロジックを追加（完全ではない） (#30)

--- app/services/export_to_csv/audio_features_service.rb
+++ app/services/export_to_csv/audio_features_service.rb
@@ -12,78 +12,123 @@ module ExportToCsv
       '幻想水滸伝ティアクライス ORIGINAL SOUNDTRACK',
       '幻想水滸伝 紡がれし百年の時 オリジナルサウンドトラック',
     ].freeze
-    EXPORTED_FILE_PATH = Rails.root.join('db/csv_files')
+
+    def execute
+      ALBUM_NAMES.each do |album_name|
+        album = album(album_name)
+        track_ids = track_ids(album)
+        rows = rows(track_ids)
+
+        exported_file_path = album_names_to_csv_filenames[album_name]
+
+        CSV.open(exported_file_path, 'w', force_quotes: true) do |csv|
+          csv << headers
+
+          rows.each do |row|
+            csv << row.values
+          end
+        end
+      end
+
+      nil
+    end
 
     def album(album_name)
       Spotify::FetchAlbumService.new.execute(album_name)
     end
 
     def track_ids(album)
-      album.tracks.map(&:id)
+      # 一度に取得できるのは 50曲 なのでオフセットを設定する必要がある
+      total_tracks = album.total_tracks
+      number_of_offset_units = (total_tracks / 50) + 1
+
+      number_of_offset_units.times.map do |i|
+        album.tracks(limit: 50, offset: i * 50).map(&:id)
+      end.flatten
     end
 
-    def execute
-      CSV.open(EXPORTED_FILE_PATH, 'w', force_quotes: true) do |csv|
-        csv << headers
-
-        rows.each do |row|
-          csv << row.values
-        end
-      end
-    end
-
-    # TODO: CSV だと順番も重要だから人間の目に優しくしたい
     def headers
       %w[
-        name
-        upc
-        href
         id
-        image_height
-        image_url
-        label
-        popularity
-        release_date
-        release_date_precision
-        total_tracks
+        duration_ms
+        tempo
+        time_signature
+        key
+        mode
+        acousticness
+        danceability
+        energy
+        instrumentalness
+        liveness
+        loudness
+        speechiness
+        valence
+        api_analysis_url
+        api_track_href
       ]
     end
 
-    def rows
+    def rows(track_ids)
       rows = []
 
-      ALBUM_NAMES.each do |album_name|
-        album = Spotify::FetchAlbumService.new.execute(album_name)
+      track_ids.each do |track_id|
+        features = Spotify::FetchAudioFeaturesService.new.execute(track_id)
 
-        name = album_name
-        upc = album.external_ids['upc']
-        href = album.href
-        id = album.id
-        # 異なる画像サイズの個数分の配列になっていて、最初が最大サイズであることはきっと保証されている
-        image_height = album.images.first['height']
-        image_url = album.images.first['url']
-        label = album.label
-        popularity = album.popularity
-        release_date = album.release_date
-        release_date_precision = album.release_date_precision
-        total_tracks = album.total_tracks
+        acousticness = features.acousticness
+        api_analysis_url = features.analysis_url
+        danceability = features.danceability
+        duration_ms = features.duration_ms
+        energy = features.energy
+        id = features.id
+        instrumentalness = features.instrumentalness
+        key = features.key
+        liveness = features.liveness
+        loudness = features.loudness
+        mode = features.mode
+        speechiness = features.speechiness
+        tempo = features.tempo
+        time_signature = features.time_signature
+        api_track_href = features.track_href
+        valence = features.valence
 
         rows << {
-          name:,
-          upc:,
-          href:,
           id:,
-          image_height:,
-          image_url:,
-          label:,
-          popularity:,
-          release_date:,
-          release_date_precision:,
-          total_tracks:
+          duration_ms:,
+          tempo:,
+          time_signature:,
+          key:,
+          mode:,
+          acousticness:,
+          danceability:,
+          energy:,
+          instrumentalness:,
+          liveness:,
+          loudness:,
+          speechiness:,
+          valence:,
+          api_analysis_url:,
+          api_track_href:
         }
       end
 
       rows
+    end
+
+    private
+
+    def album_names_to_csv_filenames
+      base_path = Rails.root.join('db/csv_files')
+
+      {
+        '幻想水滸伝 ORIGINAL GAME SOUNDTRACK' => "#{base_path}/features_s1.csv",
+        '幻想水滸伝II ORIGINAL GAME SOUNDTRACK Vol.1' => "#{base_path}/features_s2_1.csv",
+        '幻想水滸伝II ORIGINAL GAME SOUNDTRACK Vol.2' => "#{base_path}/features_s2_2.csv",
+        '幻想水滸伝III ORIGINAL SOUNDTRACK SELECTION' => "#{base_path}/features_s3.csv",
+        '幻想水滸伝IV ORIGINAL SOUNDTRACK SELECTION' => "#{base_path}/features_s4.csv",
+        '幻想水滸伝V ORIGINAL SOUNDTRACK SELECTION' => "#{base_path}/features_s5.csv",
+        '幻想水滸伝ティアクライス ORIGINAL SOUNDTRACK' => "#{base_path}/features_tk.csv",
+        '幻想水滸伝 紡がれし百年の時 オリジナルサウンドトラック' => "#{base_path}/features_tsumutoki.csv"
+      }
     end
   end
 end
