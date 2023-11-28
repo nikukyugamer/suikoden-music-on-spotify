2e5cd497301067778f5b944d9a957012ccfa4ce6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-11-28 19:13:23 +0900

    feat: 🎸 CSVファイルのエクスポートサービスを作成した (#22)

---
+++ app/services/export_to_csv/albums_service.rb
@@ -0,0 +1,86 @@
+require 'csv'
+
+module ExportToCsv
+  class AlbumsService
+    ALBUM_NAMES = [
+      '幻想水滸伝 ORIGINAL GAME SOUNDTRACK',
+      '幻想水滸伝II ORIGINAL GAME SOUNDTRACK Vol.1',
+      '幻想水滸伝II ORIGINAL GAME SOUNDTRACK Vol.2',
+      '幻想水滸伝III ORIGINAL SOUNDTRACK SELECTION',
+      '幻想水滸伝IV ORIGINAL SOUNDTRACK SELECTION',
+      '幻想水滸伝V ORIGINAL SOUNDTRACK SELECTION',
+      '幻想水滸伝ティアクライス ORIGINAL SOUNDTRACK',
+      '幻想水滸伝 紡がれし百年の時 オリジナルサウンドトラック',
+    ].freeze
+    EXPORTED_FILE_PATH = Rails.root.join('db/csv_files/albums.csv')
+
+    def execute
+      CSV.open(EXPORTED_FILE_PATH, 'w', force_quotes: true) do |csv|
+        csv << headers
+
+        rows.each do |row|
+          csv << row.values
+        end
+      end
+
+      nil
+    end
+
+    # TODO: CSV だと順番も重要だから人間の目に優しくしたい
+    def headers
+      %w[
+        name
+        upc
+        href
+        spotify_external_url
+        id
+        image_height
+        image_url
+        label
+        popularity
+        release_date
+        release_date_precision
+        total_tracks
+      ]
+    end
+
+    def rows
+      rows = []
+
+      ALBUM_NAMES.each do |album_name|
+        album = Spotify::FetchAlbumService.new.execute(album_name)
+
+        name = album_name
+        upc = album.external_ids['upc']
+        href = album.href
+        spotify_external_url = album.external_urls['spotify']
+        id = album.id
+        # NOTE: 異なる画像サイズの個数分の配列になっていて、最初が最大サイズであることはきっと保証されている
+        image_height = album.images.first['height']
+        image_url = album.images.first['url']
+        label = album.label
+        popularity = album.popularity
+        release_date = album.release_date
+        release_date_precision = album.release_date_precision
+        total_tracks = album.total_tracks
+
+        rows << {
+          name:,
+          upc:,
+          href:,
+          spotify_external_url:,
+          id:,
+          image_height:,
+          image_url:,
+          label:,
+          popularity:,
+          release_date:,
+          release_date_precision:,
+          total_tracks:
+        }
+      end
+
+      rows
+    end
+  end
+end
