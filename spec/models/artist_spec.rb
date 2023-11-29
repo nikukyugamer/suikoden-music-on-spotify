24c36e4d703a1a54341318f633c598e3c4fef558
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-11-29 23:07:49 +0900

    test: 💍 Spec を追加した

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    ImportToDb::ArtistService.new.execute
+  end
+
+  it 'レコード数が 4つ であること' do
+    expect(Artist.count).to eq 4
+  end
 end
