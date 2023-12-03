3bf40460f801f6d7317484ad0444788904729ddd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-12-03 12:43:00 +0900

    feat: 🎸 Album モデルとインポータを追加しアソシエーションを設定した (#42)
    * feat: 🎸 $ bundle exec rails g model Album
    
    * feat: 🎸 $ bundle exec rails db:migrate
    
    * feat: 🎸 db/schema.rb を更新した
    
    * feat: 🎸 マイグレーションの内容に foreign_key を追加した
    
    * feat: 🎸 albums のカラム名をより適切なものに変更した
    
    * feat: 🎸 Album のインポータを作成した
    
    * feat: 🎸 Artist と Album にアソシエーションを設定した

---
+++ app/services/import_to_db/album_service.rb
@@ -0,0 +1,39 @@
+require 'csv'
+
+module ImportToDb
+  class AlbumService
+    def initialize
+      @csv_data = CSV.read('db/csv_files/albums.csv', headers: true)
+    end
+
+    def execute
+      albums = @csv_data.map do |row|
+        artist_id_str = row['artist_id']
+
+        # Artist がインポートされていることが前提となる
+        artist = Artist.find_by(id_str: artist_id_str)
+        artist_id = artist.id
+
+        Album.new(
+          artist_id:,
+          id_str: row['id'],
+          upc: row['upc'],
+          name: row['name'],
+          label: row['label'],
+          popularity: row['popularity'].to_i,
+          total_tracks: row['total_tracks'].to_i,
+          url: row['spotify_external_url'],
+          image_url: row['image_url'],
+          image_height: row['image_height'].to_i,
+          release_date_or_year: row['release_date'],
+          release_date_precision: row['release_date_precision'],
+          api_href: row['api_href']
+        )
+      end
+
+      Album.import!(albums, on_duplicate_key_ignore: true)
+
+      nil
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_134650) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_022608) do
+  create_table "albums", force: :cascade do |t|
+    t.integer "artist_id", null: false
+    t.string "id_str", null: false
+    t.integer "upc", null: false
+    t.string "name", null: false
+    t.string "label", null: false
+    t.integer "popularity", null: false
+    t.integer "total_tracks", null: false
+    t.string "url", null: false
+    t.string "image_url", null: false
+    t.integer "image_height", null: false
+    t.string "release_date_or_year", null: false
+    t.string "release_date_precision", null: false
+    t.string "api_href", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["api_href"], name: "index_albums_on_api_href", unique: true
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+    t.index ["id_str"], name: "index_albums_on_id_str", unique: true
+    t.index ["image_url"], name: "index_albums_on_image_url", unique: true
+    t.index ["upc"], name: "index_albums_on_upc", unique: true
+    t.index ["url"], name: "index_albums_on_url", unique: true
+  end
+
   create_table "artists", force: :cascade do |t|
     t.string "id_str", null: false
     t.string "name", null: false
@@ -27,4 +51,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_134650) do
     t.index ["url"], name: "index_artists_on_url", unique: true
   end
 
+  add_foreign_key "albums", "artists"
 end
