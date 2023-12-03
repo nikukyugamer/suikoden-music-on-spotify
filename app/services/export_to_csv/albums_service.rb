d17d9c9cab8bc1c470541e4ab47997fed0b87c14
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-12-03 15:11:41 +0900

    feat: 🎸 アルバム名を YAML ファイル から読み出すようにした（そのためのアノテーションを追加） (#47)

--- app/services/export_to_csv/albums_service.rb
+++ app/services/export_to_csv/albums_service.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 module ExportToCsv
   class AlbumsService
+    # FIXME: yaml から ALBUM_NAMES を読み込むようにする
     ALBUM_NAMES = [
       '幻想水滸伝 ORIGINAL GAME SOUNDTRACK',
       '幻想水滸伝II ORIGINAL GAME SOUNDTRACK Vol.1',
