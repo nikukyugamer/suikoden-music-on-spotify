b8ba4866bceeb5159229e173205bfd512a22038b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-12-03 14:28:40 +0900

    test: 💍 Spec を追加した

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe Album, type: :model do
     expect(Album.first.tracks).to be_present
     expect(Album.first.tracks.first.class).to eq Track
   end
+
+  it 'Feature へのアソシエーションが設定されていること' do
+    expect(Album.first.features).to be_present
+    expect(Album.first.features.first.class).to eq Feature
+  end
 end
